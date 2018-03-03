ad14e662d2caa6ed9b31163706285659391898c5
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-04 00:56:20 +0100

    We're going shopping! Do you have your cart ready?

--- Recursion.playground/Contents.swift
+++ Recursion.playground/Contents.swift
@@ -4,3 +4,11 @@ indirect enum List<T> {
     case empty
     case nonEmpty(head: T, tail: List<T>)
 }
+
+let cart: List<Int> = .nonEmpty(
+    head: 4, tail: .nonEmpty(
+        head: 5, tail: .nonEmpty(
+            head: 7, tail: .nonEmpty(
+                head: 83, tail: .nonEmpty(
+                    head: 3, tail: .nonEmpty(
+                        head: 4, tail: .empty))))))
